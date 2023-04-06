e179102eb3c53f83ec4b1d74ad81780c730ecd54
William Herring <73984595+william-herring@users.noreply.github.com>
2023-04-06 13:14:48 +1000

    WCA OAuth setup

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import os
+import requests
 app = Flask(__name__)
 
 
@@ -11,3 +12,17 @@ def index():
 @app.route('/faq')
 def faq():
     return render_template('faq.html')
+
+
+@app.route('/preferences')
+def preferences():
+    code = request.args.get('code')
+    token = requests.post('https://www.worldcubeassociation.org/oauth/token', data={
+        'grant_type': 'authorization_code',
+        'code': code,
+        'client_id': os.getenv('CLIENT_ID'),
+        'client_secret': os.getenv('CLIENT_SECRET'),
+        'redirect_uri': 'http://localhost:5000/preferences'
+    }).json()['access_token']
+    print(token)
+    return render_template('preferences.html', token=token)

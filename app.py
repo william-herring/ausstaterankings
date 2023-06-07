66b5c05af2e067f50afea6f4ea9fdd8d5fc7450f
William Herring <73984595+william-herring@users.noreply.github.com>
2023-06-07 17:39:36 +1000

    Final bug fixes

--- app.py
+++ app.py
@@ -113,14 +113,43 @@ def account_redirect():
             'personal_records']
         results = []
         for r in raw_results.keys():
-            result = Result(
-                event=r,
-                single=raw_results[r]['single']['best'],
-                average=raw_results[r]['average']['best'],
-                single_rank=raw_results[r]['single']['world_rank'],
-                average_rank=raw_results[r]['average']['world_rank'],
-                person_id=user.id
-            )
+            if r == '333mbf':
+                continue
+
+            try:
+                single = raw_results[r]['single']['best']
+            except KeyError:
+                single = None
+            try:
+                average = raw_results[r]['average']['best']
+            except KeyError:
+                average = None
+
+            # Validate that user has single/average results (not DNF)
+            if single is not None and average is not None:
+                result = Result(
+                    event=r,
+                    single=single,
+                    average=average,
+                    single_rank=raw_results[r]['single']['world_rank'],
+                    average_rank=raw_results[r]['average']['world_rank'],
+                    person_id=user.id
+                )
+            elif single is not None:
+                result = Result(
+                    event=r,
+                    single=single,
+                    single_rank=raw_results[r]['single']['world_rank'],
+                    person_id=user.id
+                )
+            else:
+                result = Result(
+                    event=r,
+                    average=average,
+                    average_rank=raw_results[r]['average']['world_rank'],
+                    person_id=user.id
+                )
+
             results.append(result)
 
         user.results = results

2a327855d4408bbd283283b437ebc608b1058ae2
William Herring <73984595+william-herring@users.noreply.github.com>
2023-06-11 13:52:31 +1000

    added admins

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def account_redirect():
 
 @app.route('/manual-entry')
 def manual_entry():
-    if session['user']['wca_id'] == '2019HERR14':
+    if session['user']['wca_id'] in ['2019HERR14', '2018NGHA02', '2019LUCA01']:
         wca_id = request.args.get('wca_id')
         state = request.args.get('state')
 

bb0d4440a5f215e2113324df4e661825249805e0
William Herring <73984595+william-herring@users.noreply.github.com>
2023-06-16 17:40:03 +1000

    fixed scheduled result updates

--- update_results.py
+++ update_results.py
@@ -17,16 +17,45 @@ def update_results():
             'personal_records']
         results = []
         for r in raw_results.keys():
-            result = Result(
-                event=r,
-                single=raw_results[r]['single']['best'],
-                average=raw_results[r]['average']['best'],
-                single_rank=raw_results[r]['single']['world_rank'],
-                average_rank=raw_results[r]['average']['world_rank'],
-                person_id=person.id
-            )
+            if r in ['333mbf', '333ft', 'magic', 'mmagic']:
+                continue
+
+            try:
+                single = raw_results[r]['single']['best']
+            except KeyError:
+                single = None
+            try:
+                average = raw_results[r]['average']['best']
+            except KeyError:
+                average = None
+
+            # Validate that user has single/average results (not DNF)
+            if single is not None and average is not None:
+                result = Result(
+                    event=r,
+                    single=single,
+                    average=average,
+                    single_rank=raw_results[r]['single']['world_rank'],
+                    average_rank=raw_results[r]['average']['world_rank'],
+                    person_id=person.id
+                )
+            elif single is not None:
+                result = Result(
+                    event=r,
+                    single=single,
+                    single_rank=raw_results[r]['single']['world_rank'],
+                    person_id=person.id
+                )
+            else:
+                result = Result(
+                    event=r,
+                    average=average,
+                    average_rank=r[r]['average']['world_rank'],
+                    person_id=person.id
+                )
+
             results.append(result)
 
         person.results = results
-
-    db.session.commit()
+        db.session.add(person)
+        db.session.commit()

4cc85b038f2de2c86a8e876c68a95a1712da3409
William Herring <73984595+william-herring@users.noreply.github.com>
2025-01-07 16:44:13 +1100

    redesign

--- static/src/js/prefs.js
+++ static/src/js/prefs.js
@@ -13,9 +13,9 @@ function submit(e) {
     }).then(response => {
         let messageBox = document.getElementById("messageBox");
         if (response.status == 200) {
-            messageBox.innerHTML = "<p class='text-center mt-4 text-green-700 text-lg'>Updated preferences</p>"
+            messageBox.innerHTML = "<p class='mt-4 text-lg'>Updated preferences</p>"
         } else {
-            messageBox.innerHTML = "<p class='text-center mt-4 text-red-700 text-lg'>Something went wrong</p>"
+            messageBox.innerHTML = "<p class='tmt-4 text-red-600 text-lg'>Something went wrong</p>"
         }
     });
 }
